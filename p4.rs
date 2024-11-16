a948c773ecc39d187f7785d5b8c9863123ff8743
Leandro Coutinho <lescoutinhovr@gmail.com>
2024-11-16 15:43:37 -0300

    Solved problem 4 part 2

--- p4.rs
+++ p4.rs
@@ -13,14 +13,24 @@ fn sorted(s: &str) -> String {
     chars.into_iter().collect()
 }
 
+fn freq(s: &str) -> HashMap<char, usize> {
+    let mut map = HashMap::new();
+    s.chars().for_each(|c| { map.entry(c).and_modify(|qt| *qt += 1).or_insert(1); });
+    map
+}
+
 fn p1(a: i32, b: i32) -> usize {
     (a..=b).map(|n| n.to_string()).filter(|s| set(&s).len() < s.len() && &sorted(&s) == s).count()
+}
+
+fn p2(a: i32, b: i32) -> usize {
+    (a..=b).map(|n| n.to_string()).filter(|s| &sorted(&s) == s && freq(&s).values().find(|&&qt| qt == 2).is_some()).count()
 }
 
 
 fn main() {
     assert_eq!(1764, p1(152085, 670283));
-    // assert_eq!(0, p2(IN));
+    assert_eq!(1196, p2(152085, 670283));
 }
 
 
