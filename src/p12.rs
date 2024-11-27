83fd17ba9f20e775bc976f3ba092a4b44d5c6c7b
tivrfoa <lescoutinhovr@gmail.com>
2024-11-27 06:10:41 -0300

    velocity is force?!

--- src/p12.rs
+++ src/p12.rs
@@ -1,25 +1,76 @@
 use std::collections::{HashMap, HashSet};
 
-fn get_moons(input: &str) -> [(i32, i32, i32); 4] {
-    let mut moons: [(i32, i32, i32); 4] = [(0, 0, 0); 4];
+#[derive(Debug)]
+struct Moon {
+    x: i32,
+    y: i32,
+    z: i32,
+    vx: i32,
+    vy: i32,
+    vz: i32,
+    force_x: i32,
+    force_y: i32,
+    force_z: i32,
+}
 
-    for (i, line) in input.lines().enumerate() {
-        let t1: Vec<&str> = line[1..line.len() - 1].split(", ").collect();
-        let a: i32 = t1[0].split_once('=').unwrap().1.parse().unwrap();
-        let b: i32 = t1[1].split_once('=').unwrap().1.parse().unwrap();
-        let c: i32 = t1[2].split_once('=').unwrap().1.parse().unwrap();
-        moons[i] = (a, b, c);
+impl Moon {
+    fn new(t: (i32, i32, i32)) -> Self {
+        Self {
+            x: t.0,
+            y: t.1,
+            z: t.2,
+            vx: 0,
+            vy: 0,
+            vz: 0,
+            force_x: 0,
+            force_y: 0,
+            force_z: 0,
+        }
     }
+}
 
-    moons
+fn get_moons(input: &str) -> Vec<Moon> {
+    input
+        .lines()
+        .map(|line| {
+            let t1: Vec<&str> = line[1..line.len() - 1].split(", ").collect();
+            let a: i32 = t1[0].split_once('=').unwrap().1.parse().unwrap();
+            let b: i32 = t1[1].split_once('=').unwrap().1.parse().unwrap();
+            let c: i32 = t1[2].split_once('=').unwrap().1.parse().unwrap();
+            Moon::new((a, b, c))
+        })
+    .collect()
 }
 
 pub fn p1(input: &str) -> i32 {
     let mut total = 0;
     let mut moons = get_moons(input);
-    dbg!(moons);
+    let len = moons.len();
+    // dbg!(moons);
 
-    for i in 0..1000 {
+    for step in 0..1000 {
+
+        // calc gravity
+        for i in 0..len {
+            for j in 0..len {
+                if i == j { continue; }
+
+                moons[i].force_x += apply_gravity(moons[i].x, moons[j].x);
+                moons[i].force_y += apply_gravity(moons[i].y, moons[j].y);
+                moons[i].force_z += apply_gravity(moons[i].z, moons[j].z);
+            }
+        }
+
+        // apply forces
+        for i in 0..len {
+            moons[i].x += moons[i].force_x;
+            moons[i].force_x = 0;
+            moons[i].y += moons[i].force_y;
+            moons[i].force_y = 0;
+            moons[i].z += moons[i].force_z;
+            moons[i].force_z = 0;
+        }
+
     }
 
     total
@@ -32,15 +83,6 @@ fn apply_gravity(a: i32, b: i32) -> i32 {
         a - b
     }
 }
-
-fn process_input(input: &str) -> Vec<Vec<char>> {
-    input
-        .trim()
-        .lines()
-        .map(|line| line.chars().collect())
-        .collect()
-}
-
 
 #[cfg(test)]
 mod tests {
