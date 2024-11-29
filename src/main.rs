7d79bfd68dc7d60c9a5bc6c76da5a9e3104ee7f4
tivrfoa <lescoutinhovr@gmail.com>
2024-11-29 09:00:17 -0300

    solved day 13 part 1

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,14 @@ mod intcode;
 mod p9;
 mod p10;
 mod p11;
-mod p12;
+// mod p12; // slow
+mod p13;
 mod util;
 
-use p12 as Main;
+use p13 as Main;
 
 fn main() {
-    Main::p1(Main::IN, 1000);
+    Main::p1(Main::IN);
     // Main::p2(Main::IN);
-
-    // run_previous();
 }
 
-fn run_previous() {
-    p9::p1(p9::IN);
-    p9::p2(p9::IN);
-    p10::p1(p10::IN);
-    p10::p2(p10::IN);
-}
