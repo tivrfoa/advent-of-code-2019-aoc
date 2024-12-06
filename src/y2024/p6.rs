6557f05a0ad127c25def6cdcf0e2890999cd3e49
tivrfoa <lescoutinhovr@gmail.com>
2024-12-06 09:21:46 -0300

    slow but solved y2024 day 6 part 2

--- src/y2024/p6.rs
+++ src/y2024/p6.rs
@@ -62,10 +62,82 @@ pub fn p1(input: &str) -> usize {
     }
 }
 
+fn is_loop(start_pos: (usize, usize), grid: &Vec<Vec<char>>) -> bool {
+    let mut visited = HashSet::new();
+    let rows = grid.len() as i32;
+    let cols = grid[0].len() as i32;
+    let (mut r, mut c) = start_pos;
+    let mut dir = N;
+
+    loop {
+        if !visited.insert((r, c, dir)) {
+            return true;
+        }
+
+        loop {
+            let ir = r as i32 + M[dir].0;
+            let ic = c as i32 + M[dir].1;
+    
+            if ir < 0 || ir == rows || ic < 0 || ic == cols {
+                return false;
+            }
+
+            if grid[ir as usize][ic as usize] == '#' {
+                dir = T[dir];
+            } else {
+                (r, c) = (ir as usize, ic as usize);
+                break;
+            }
+        }
+    }
+}
+
+fn get_path(grid: &Vec<Vec<char>>) -> ((usize, usize), HashSet<(usize, usize)>) {
+    let mut visited = HashSet::new();
+    let rows = grid.len() as i32;
+    let cols = grid[0].len() as i32;
+    let start_pos = get_start_pos(&grid);
+    let (mut r, mut c) = start_pos;
+    let mut dir = N;
+
+    loop {
+        visited.insert((r, c));
+
+        loop {
+            let ir = r as i32 + M[dir].0;
+            let ic = c as i32 + M[dir].1;
+    
+            if ir < 0 || ir == rows || ic < 0 || ic == cols {
+                return (start_pos, visited);
+            }
+
+            if grid[ir as usize][ic as usize] == '#' {
+                dir = T[dir];
+            } else {
+                (r, c) = (ir as usize, ic as usize);
+                break;
+            }
+        }
+    }
+}
+
+/// Strategy:
+///     - find the p1 path
+///     - need to check for every poosition in that path (excluding start position)
+///     - count how many of them become a loop
 pub fn p2(input: &str) -> usize {
+    let mut qt = 0;
+    let mut grid = input.lines().map(|l| l.chars().collect::<Vec<char>>()).collect::<Vec<_>>();
+    let (start_pos, path) = get_path(&grid);
 
+    for p in path {
+        if p == start_pos { continue; }
+        grid[p.0][p.1] = '#';
+        if is_loop(start_pos, &grid) { qt += 1; }
+        grid[p.0][p.1] = '.';
+    }
 
-    0
+    qt
 }
 
 
@@ -85,9 +157,13 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
+    fn test_p2_sample() {
+        assert_eq!(6, p2(SAMPLE));
+    }
+
+    #[test]
     fn test_p2() {
-        assert_eq!(171, p2(IN));
+        assert_eq!(2262, p2(IN));
     }
 }
 
