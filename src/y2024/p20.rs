db1786fae115e75d1293bac073711d734f16b5e0
tivrfoa <lescoutinhovr@gmail.com>
2025-01-04 11:56:35 -0300

    ported Errichto part 2

--- src/y2024/p20.rs
+++ src/y2024/p20.rs
@@ -62,9 +62,49 @@ pub fn p1(input: &str) -> usize {
 }
 
 pub fn p2(input: &str) -> usize {
+    let mut answer = 0;
+    let g = input_to_char_grid(input);
+    let rows = g.len();
+    let cols = g[0].len();
+    let (start_pos, end_pos) = {
+        let (mut start_pos, mut end_pos) = ((0, 0), (0, 0));
+        for (r, row) in g.it() {
+            for (c, v) in row.it() {
+                if *v == 'S' {
+                    start_pos = (r, c);
+                }
+                if *v == 'E' {
+                    end_pos = (r, c);
+                }
+            }
+        }
+        (start_pos, end_pos)
+    };
+    let A = get_min_distances(start_pos, &g);
+    let B = get_min_distances(end_pos, &g);
+    let M = 20;
 
+    let normal = A[end_pos.0][end_pos.1];
+    for r in 0..rows {
+        for c in 0..cols {
+            if g[r][c] == '#' { continue; }
+            for nr in if r < M { 0 } else { r - M }..=(rows - 1).min(r + M) {
+                for nc in if c < M { 0 } else { c - M }..=(cols - 1).min(c + M) {
+                    let abs_diff = r.abs_diff(nr) + c.abs_diff(nc);
+                    if abs_diff <= M {
+                        if g[nr][nc] != '#' && B[nr][nc] != INF {
+                            let here = A[r][c] + B[nr][nc] + abs_diff;
+                            if here <= normal - 100 {
+                                answer += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-    0
+    answer
 }
 
 
@@ -90,9 +130,8 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_p2_in() {
-        assert_eq!(171, p2(IN));
+        assert_eq!(977747, p2(IN));
     }
 }
 
